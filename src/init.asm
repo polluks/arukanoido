d4013e2e6f59e502af2409893e2298d2738320a9
Sven Michael Klose <pixel@copei.de>
2015-08-10 14:36:37 +0200

    Set auxiliary color.

--- src/init.asm
+++ src/init.asm
@@ -9,7 +9,9 @@ main:
     sta $9003
     lda #@(+ vic_screen_1e00 vic_charset_1000)
     sta $9005
-    lda #@(+ reverse red)  ; Screen and border color.
+    lda #@(* light_cyan 16) ; Auxiliary color.
+    sta $900d
+    lda #@(+ reverse red)   ; Screen and border color.
     sta $900f
 
 forever:jmp forever
