2ae9fe87e0c74139aecec01b85f91747e11385bb
Sven Michael Klose <pixel@copei.de>
2015-08-10 16:19:37 +0200

    Clear the screen.

--- src/main.asm
+++ src/main.asm
@@ -1,6 +1,13 @@
 start:
     ; Clear screen.
+    lda #0
+    tax
+l:  sta screen,x
+    sta @(+ 256 screen),x
+    dex
+    bne -l
     ; Draw border.
     ; Unpack bricks.
+
 forever:
     jmp forever
